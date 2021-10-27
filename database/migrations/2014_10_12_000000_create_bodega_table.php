5c2b0de63b651d94857f226dc504a5d66696485d
AndyCaro78 <91231999+AndyCaro78@users.noreply.github.com>
2021-10-26 23:25:33 -0500

    otro

---
+++ database/migrations/2014_10_12_000000_create_bodega_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBodegaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bodega', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre_Producto');
+            $table->string('Codigo_produc')->unique()->nullable();
+            $table->string('Numero_Cajas');
+            $table->Interger('Edad_Minima'); 
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
