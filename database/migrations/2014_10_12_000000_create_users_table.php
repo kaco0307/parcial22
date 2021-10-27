b4337f53317b708aa9a4f53ead84004ba03da19f
AndyCaro78 <91231999+AndyCaro78@users.noreply.github.com>
2021-10-26 22:15:46 -0500

    hjsghjgdh
    sjhghsdg

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,17 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->Interger('edad'); 
+            $table->String('Tipo_usuario');
+            $table->String('Dinero');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->system();
+              uhdsjhshhsjdgjhsbjdbjhshj
+
+
+            pruebayu
         });
     }
 
