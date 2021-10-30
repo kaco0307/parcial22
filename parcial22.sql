9cca1345934ce92098bcbe88c2908eccfd1c09d3
kaco0307 <pjelfenomeno15@gmail.com>
2021-10-30 15:16:11 -0500

    actu BD

--- parcial22.sql
+++ parcial22.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-10-2021 a las 04:55:53
+-- Tiempo de generación: 30-10-2021 a las 22:14:56
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.5
 
@@ -24,6 +24,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `bodegas`
+--
+
+CREATE TABLE `bodegas` (
+  `Codigo_produc` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Nombre_Producto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Numero_Cajas` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Edad_Minima` int(11) NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `bodegas`
+--
+
+INSERT INTO `bodegas` (`Codigo_produc`, `Nombre_Producto`, `Numero_Cajas`, `Edad_Minima`, `remember_token`, `created_at`, `updated_at`) VALUES
+('13', 'ferepuche', '15', 18, NULL, '2021-10-30 13:08:22', '2021-10-30 13:36:50'),
+('85', 'manzanas', '30', 13, NULL, '2021-10-30 13:28:58', '2021-10-30 13:28:58');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `migrations`
 --
 
@@ -38,7 +78,42 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(2, '2014_10_12_000000_create_users_table', 1);
+(9, '2014_10_12_000000_create_bodegas_table', 3),
+(11, '2014_10_12_000000_create_users_table', 4),
+(12, '2014_10_12_100000_create_password_resets_table', 4),
+(13, '2019_08_19_000000_create_failed_jobs_table', 4),
+(14, '2019_12_14_000001_create_personal_access_tokens_table', 4),
+(15, '2021_10_30_072040_bodegas', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -65,19 +140,47 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `edad`, `Tipo_usuario`, `Dinero`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'andrea', 'andrea@gmail.com', NULL, '$2y$10$WS7p7ZTn4ScFwTw0SX4ts.U/fszYPVx1hV3ZGYjbLzUvCGcf2MDua', 21, '02', '1000000', NULL, '2021-10-28 06:44:04', '2021-10-28 06:44:04'),
-(2, 'jaime', 'jaime42@gmail.com', NULL, '$2y$10$vAMoyMP9vXVzyXzNYTxXtueoTUEd.swY8rh5fyltnxZbalkPm1QEG', 15, '03', '15000', NULL, '2021-10-28 06:53:16', '2021-10-28 06:53:16'),
-(10, 'javier', 'javierm12@gmail.com', NULL, '$2y$10$5lwbonASgEC1Xb950VrH5eCn8864kkRsgznG.vys9cMEUpEOevdda', 15, '03', '15', NULL, '2021-10-28 07:40:59', '2021-10-28 07:40:59');
+(1, 'andrea', 'andrea@gmail.com', NULL, '$2y$10$GIiqz4YjHUQWN3mCrAsi2OwnoHRqwmN7bFJOwGcPzz1/.zDua9Wj.', 23, '02', '12000', NULL, '2021-10-30 12:35:16', '2021-10-30 12:35:16'),
+(2, 'jairo98', 'jairo98@gmail.com', NULL, '$2y$10$yr6EVbQ3.ucpRl13gVfKrOQENVhAR61hSgyKJdHhyYMnoXBPhsK/K', 75, NULL, '12000', NULL, '2021-10-30 13:44:05', '2021-10-30 13:44:05'),
+(3, 'jaime', 'jaime@gmail.com', NULL, '$2y$10$mwAMwX.fFJbMNlvRUajHWeSLEEFbvgPvjdlQeppDztV72tcrG1YqS', 52, '02', '12000', NULL, '2021-10-30 13:47:11', '2021-10-30 13:47:11'),
+(4, 'gabriel', 'gabriel@gmail.com', NULL, '$2y$10$eFc6vOz19B1qphv87hEp1OjnyEJ40qA3hY/0aRAGK90IcAK4MtJ5G', 15, '02', '5220', NULL, '2021-10-30 13:50:01', '2021-10-30 13:50:01');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `bodegas`
+--
+ALTER TABLE `bodegas`
+  ADD PRIMARY KEY (`Codigo_produc`);
+
+--
+-- Indices de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
 -- Indices de la tabla `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
 -- Indices de la tabla `users`
@@ -91,16 +194,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
